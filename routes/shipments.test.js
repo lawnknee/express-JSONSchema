6ab29e25237e525f669cb2328134f7bf3f753657
Loni <kuangloni@gmail.com>
2021-08-03 17:32:37 -0700

    mock function for shipProduct on shipments.test

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -1,11 +1,15 @@
 "use strict";
 
+const shipItAPI = require("../shipItApi");
+shipItAPI.shipProduct = jest.fn();
+
 const request = require("supertest");
 const app = require("../app");
 
-
-describe("POST /", function () {
+describe("POST /shipments", function () {
   test("valid", async function () {
+    shipItAPI.shipProduct.mockReturnValue(2000);
+
     const resp = await request(app).post("/shipments").send({
       productId: 1000,
       name: "Test Tester",
@@ -13,7 +17,7 @@ describe("POST /", function () {
       zipcode: "12345-6789",
     });
 
-    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+    expect(resp.body).toEqual({ shipped: 2000 });
   });
 
   test("invalid input", async function () {
@@ -27,6 +31,10 @@ describe("POST /", function () {
       });
 
     expect(resp.statusCode).toEqual(400);
-    expect(resp.body).toEqual({ error: { message: ["instance.productId must be greater than or equal to 1000"], status: 400 }})
+    expect(resp.body).toEqual({ 
+      error: { 
+        message: ["instance.productId must be greater than or equal to 1000"], 
+        status: 400 }
+      })
   })
 });
