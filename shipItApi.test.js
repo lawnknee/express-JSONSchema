8d73c0e683b5d99fb1f1b018763265262804881e
lillianlakes <lillian.lakes@gmail.com>
2021-08-03 17:22:54 -0700

    adds test for shipItApi

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,17 +1,34 @@
 "use strict";
 
-const {
-  shipProduct,
-} = require("./shipItApi");
+const AxiosMockAdapter = require("axios-mock-adapter");
+const axios = require("axios");
+const axiosMock = new AxiosMockAdapter(axios);
 
+const {shipProduct, SHIPIT_SHIP_URL, SHIPIT_API_KEY } = 
+require("./shipItApi");
+
+const { response } = require("express");
 
 test("shipProduct", async function () {
-  const shipId = await shipProduct({
-    productId: 1000,
+
+  const responseData = { receipt: {
+    itemId: 1000,
     name: "Test Tester",
     addr: "100 Test St",
     zip: "12345-6789",
-  });
+    shipId: 2000
+  }};
 
-  expect(shipId).toEqual(expect.any(Number));
+  axiosMock.onPost(`${SHIPIT_SHIP_URL}`)
+    .reply(201, responseData);
+
+    const shipId = await shipProduct({
+      productId: 1000,
+      name: "Test Tester",
+      addr: "100 Test St",
+      zip: "12345-6789",
+    });
+
+  expect(shipId).toEqual(2000);
+  expect(responseData.receipt.shipId).toEqual(shipId);
 });
